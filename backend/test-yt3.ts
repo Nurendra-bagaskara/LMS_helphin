28019f6275b808a4d8daa42fa550daff813cf96c
Nurendra-bagaskara <bagaskara636@gmail.com>
2026-04-11 14:31:00 +0700

    cleanCode

--- backend/test-yt3.ts
+++
@@ -1,7 +0,0 @@
-import { fetch } from "bun";
-async function testAPI() {
-    const res = await fetch("https://yt.lemnoslife.com/videos?part=chapters&id=b3dxMwIO-yM");
-    const data = await res.json();
-    console.log(JSON.stringify(data, null, 2));
-}
-testAPI();
--- backend/test_pw.js
+++
@@ -1,1 +0,0 @@
-const bcrypt = require('bcryptjs'); // Need bcryptjs or similar since Bun is not available in node. Wait, we can use Elysia's backend env. Let's just create a Bun script.
